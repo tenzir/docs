bbbacbefe4dc459c9c7007b585664ca01a5361dd
Dominik Lohmann <mail@dominiklohmann.de>
2025-05-31 18:00:49 +0200

    Make the docs structure interactive

--- markdoc.config.mjs
+++ markdoc.config.mjs
@@ -1,6 +1,15 @@
-import { defineMarkdocConfig } from "@astrojs/markdoc/config";
+import { defineMarkdocConfig, component } from "@astrojs/markdoc/config";
 import starlightMarkdoc from "@astrojs/starlight-markdoc";
 
 export default defineMarkdocConfig({
   extends: [starlightMarkdoc()],
+  tags: {
+    InteractiveDocsStructure: {
+      render: component("./src/components/InteractiveDocsStructure.astro"),
+      attributes: {
+        imageSrc: { type: String },
+        activeSection: { type: String },
+      },
+    },
+  },
 });
