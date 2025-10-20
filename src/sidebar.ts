688a6dea8fed17f991936f75dc1ce71d7439fe67
Matthias Vallentin <matthias@vallentin.net>
2025-10-20 14:16:35 +0200

    Rewire AWS instructions

--- src/sidebar.ts
+++ src/sidebar.ts
@@ -24,8 +24,8 @@ export const guides = [
     collapsed: true,
     items: [
       "guides/platform-setup",
-      "guides/platform-setup/choose-a-scenario",
       "guides/platform-setup/deploy-on-aws",
+      "guides/platform-setup/choose-a-scenario",
       "guides/platform-setup/configure-reverse-proxy",
       "guides/platform-setup/configure-internal-services",
       "guides/platform-setup/configure-identity-provider",
