38d8ba42cc3b7816dfb830b90e6e36fcc3d85c8a
Matthias Vallentin <matthias@vallentin.net>
2026-05-15 21:22:17 +0200

    Document Amazon Kinesis operators

--- src/sidebar.ts
+++ src/sidebar.ts
@@ -335,6 +335,7 @@ export const integrations = [
         collapsed: true,
         items: [
           "integrations/amazon",
+          "integrations/amazon/kinesis",
           "integrations/amazon/msk",
           "integrations/amazon/s3",
           "integrations/amazon/security-lake",
